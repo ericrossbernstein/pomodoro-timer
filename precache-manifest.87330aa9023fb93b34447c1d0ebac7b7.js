f6cd4fbb9a6a071291af337c50b2c951a464d0b4
Eric Ross Bernstein <ericrossbernstein@gmail.com>
2021-03-18 17:26:49 -0700

    Updates

---
+++ precache-manifest.87330aa9023fb93b34447c1d0ebac7b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b83551c5c1cb09d095296d9ec05974f",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "1ce9aa2ba38da10862d6",
+    "url": "/pomodoro-timer/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "226820f6af32e486a61f",
+    "url": "/pomodoro-timer/static/js/2.0885f580.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/pomodoro-timer/static/js/2.0885f580.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ce9aa2ba38da10862d6",
+    "url": "/pomodoro-timer/static/js/main.3840d153.chunk.js"
+  },
+  {
+    "revision": "5b934f73dc3f50a7b525",
+    "url": "/pomodoro-timer/static/js/runtime-main.cb3a558a.js"
+  }
+]);
